045e293fa1ec8afd611aeea302cdf6326878c9a2
Sriram-data-engg <sriram.data.engineers@gmail.com>
2024-07-28 09:02:36 +0530

    Assigment Behavoir

--- sample_set1/program1.py
+++ sample_set1/program1.py
@@ -1,3 +1,11 @@
-print('Hi')
-cv=100
-print(cv)
+###Assignment Behaviour###
+'''
+Assigning a object to a new variable is pass by value or pass by reference
+
+Testing in 1. Simple aassignment 2.Passing to method
+'''
+
+a=10
+b=a
+a=30
+print('value of $a')
